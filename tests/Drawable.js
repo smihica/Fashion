b2010a9b8bc0c846b2ee99b338c3ad49f7c17467
Moriyoshi Koizumi <mozo@mozo.jp>
2012-04-18 16:31:56 +0900

    More tests.

--- tests/Drawable.js
+++ tests/Drawable.js
@@ -30,9 +30,27 @@ exports.group = {
   testDraw: function(test) {
     test.expect(3);
     var d = new Fashion.Drawable();
-    test.equals(d.draw({ __id: null }).__id, 'G' + d._id_acc);
-    test.equals(d.draw({ __id: null }).__id, 'G' + d._id_acc);
-    test.equals(d.draw({ __id: null }).__id, 'G' + d._id_acc);
+    test.equals(d.draw({}).__id, 'G' + d._id_acc);
+    test.equals(d.draw({}).__id, 'G' + d._id_acc);
+    test.equals(d.draw({}).__id, 'G' + d._id_acc);
+    test.done();
+  },
+
+  testMap1: function(test) {
+    test.expect(1);
+    var d = new Fashion.Drawable();
+    var shapes = [ d.draw({}), d.draw({}), d.draw({}) ];
+    var _shapes = d.map(function(shape) { return shape; });
+    test.deepEqual(_shapes, shapes);
+    test.done();
+  },
+
+  testMap2: function(test) {
+    test.expect(1);
+    var d = new Fashion.Drawable();
+    var shapes = [ d.draw({ a:0 }), d.draw({ a:1 }), d.draw({ a:2 }) ];
+    var _shapes = d.map(function(shape) { return shape.a != 1 ? shape.a: void(0); });
+    test.deepEqual(_shapes, [ 0, 2 ]);
     test.done();
   }
 };
