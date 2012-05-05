a6337981d7968d7d90fecec006589f88317c6ebf
Moriyoshi Koizumi <mozo@mozo.jp>
2012-05-06 06:08:54 +0900

    Fix CSS parser.

--- src/lib/css.js
+++ src/lib/css.js
@@ -1,16 +1,19 @@
 var _parseCSSRules = (function () {
-  var regexp_for_rules = /\s*(-?(?:[_a-z\u00a0-\u10ffff]|\\[^\n\r\f])(?:[\-_a-z\u00a0-\u10ffff]|\\[^\n\r\f])*)\s*:\s*((?:(?:(?:[^;\\\n\r\f\t "']|\\[^\n\r\f\t ])+|"(?:[^\n\r\f\\"]|\\(?:\n|\r\n|\r|\f)|\\[^\n\r\f])*"|'(?:[^\n\r\f\\']|\\(?:\n|\r\n|\r|\f)|\\[^\n\r\f])*')(?:\s+|(?=;|$)))+)(?:;|$)/g;
+  var regexp_for_rules = /\s*(-?(?:[_a-z\u00a0-\u10ffff]|\\[^\n\r\f#])(?:[\-_A-Za-z\u00a0-\u10ffff]|\\[^\n\r\f])*)\s*:\s*((?:(?:(?:[^;\\ \n\r\t\f"']|\\[0-9A-Fa-f]{1,6}(?:\r\n|[ \n\r\t\f])?|\\[^\n\r\f0-9A-Fa-f])+|"(?:[^\n\r\f\\"]|\\(?:\n|\r\n|\r|\f)|\\[^\n\r\f])*"|'(?:[^\n\r\f\\']|\\(?:\n|\r\n|\r|\f)|\\[^\n\r\f])*')(?:\s+|(?=;|$)))+)(?:;|$)/g;
 
-  var regexp_for_values = /(?:((?:[^;\\\n\r\f\t "']|\\[^\n\r\f\t ])+)|"((?:[^\n\r\f\\"]|\\(?:\n|\r\n|\r|\f)|\\[^\n\r\f])*)"|'((?:[^\n\r\f\\']|\\(?:\n|\r\n|\r|\f)|\\[^\n\r\f])*)')(?:\s+|$)/g;
+  var regexp_for_values = /(?:((?:[^;\\ \n\r\t\f"']|\\[0-9A-Fa-f]{1,6}(?:\r\n|[ \n\r\t\f])?|\\[^\n\r\f0-9A-Fa-f])+)|"((?:[^\n\r\f\\"]|\\(?:\n|\r\n|\r|\f)|\\[^\n\r\f])*)"|'((?:[^\n\r\f\\']|\\(?:\n|\r\n|\r|\f)|\\[^\n\r\f])*)')(?:\s+|$)/g;
 
   function unescape(escaped) {
-    return escaped.replace(/\\(0{0,2}d[89ab][0-9a-f]{2})\\(0{0,2}d[c-f][0-9a-f]{2})|\\([0-9a-f]{1,6})|\\([^\n\r\f0-9a-f])/g, function(_, a, b, c, d) {
+    return escaped.replace(/(?:\\(0{0,2}d[89ab][0-9A-Fa-f]{2})(?:\r\n|[ \n\r\t\f])?)?\\([0-9A-Fa-f]{1,6})(?:\r\n|[ \n\r\t\f])?|\\([^\n\r\f0-9A-Fa-f])/g, function(_, a, b, c) {
       if (a !== void(0)) {
-        return String.fromCharCode((((parseInt(a, 16) & 0x3fff) << 12) | (parseInt(b, 16) & 0x3fff)) + 0x10000);
+        var c2 = parseInt(b, 16) ;
+        if (c2 < 0xdc00 || c2 > 0xdfff)
+          throw new ValueError("Invalid surrogate pair");
+        return String.fromCharCode((((parseInt(a, 16) & 0x3ff) << 10) | (c2 & 0x3ff)) + 0x10000);
+      } else if (b !== void(0)) {
+        return String.fromCharCode(parseInt(b, 16));
       } else if (c !== void(0)) {
-        return String.fromCharCode(parseInt(c, 16));
-      } else if (d !== void(0)) {
-        return d;
+        return c;
       }
     });
   }
